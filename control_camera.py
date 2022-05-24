8621da326499f8b69dffbee92a973ae7c602123f
XiaoleiDu <59750876+XiaoleiDu@users.noreply.github.com>
2022-05-24 16:00:49 -0700

    Added New Camera Control file and usb file
    The script will autorun at startup on a raspberry Pi with rc.local file modified and can write pictures to new folders each time the program runs

--- control_camera.py
+++ control_camera.py
@@ -1,8 +1,9 @@
+#! /usr/bin/env python
 import RPi.GPIO as GPIO
 import time
 import cv2
-
-
+import usb_trial
+import os
 GPIO.setwarnings(False) 
 GPIO.setmode(GPIO.BOARD) 
 GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
@@ -12,13 +13,33 @@ cap = cv2.VideoCapture(0) # video capture source camera (Here webcam of laptop)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)  # set new dimensionns to cam object (not cap)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
 
-
+i = 1
+folder = 1
+j = 1
 while True: 
     if GPIO.input(10) == GPIO.HIGH:
-        print("Button was pushed!")
+        print("Button was pushed!")  
+        root = str((usb_trial.get_mount_points()[0][1]))
+        print(root)
+        #root = '/home/pi/Desktop/fixedwing-mangrove/'
+        path = root + '/folder'+str(folder)+'/'
+        
+        while(os.path.isdir(path)&j==1):
+            fold_dir = 'folder'+str(folder)
+            #print("%s exists",fold_dir)
+            folder+=1
+            path = root +'folder'+str(folder)
+        
+        if (j!=2):
+            os.mkdir(path)
+            os.chdir(path)
+        j =2 
+        
+            
         ret,frame = cap.read() # return a single frame in variable `frame`
-        cv2.imwrite('1.png',frame)
+        cv2.imwrite(str(i)+'.png',frame)
         time.sleep(1)
+        i+=1
 
 
 
